b822a259d24ca016b7da8b81bb1ba98d902e534b
Vasar007 <vasar007@yandex.ru>
2018-10-12 23:55:24 +0300

    Add exit commands in build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,6 +5,6 @@ set -x
 mkdir build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION -DTARGET_CPU=$TARGET_CPU
-cmake --build .
-ctest --output-on-failure
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION -DTARGET_CPU=$TARGET_CPU || exit 1
+cmake --build . || exit 1
+ctest --output-on-failure || exit 1
